9a95bb50456c66916e1ca35f0efe5f45a4d69c3d
mr-akami <shun_murakami@earthbrain.com>
2024-03-22 22:47:46 +0900

    chore: setup vitest

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { AppController } from './app.controller.ts';
+import { AppService } from './app.service.ts';
 
 describe('AppController', () => {
   let appController: AppController;
